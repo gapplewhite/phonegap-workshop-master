ae456bf41b01126383b1101cf8674a42e6f27da1
gapplewhite <gapplewhite@gmail.com>
2013-10-14 20:51:57 -0500

    Ecool Cargar toda BD con fotos

--- phonegap-workshop-master/js/HomeView.js
+++ phonegap-workshop-master/js/HomeView.js
@@ -6,9 +6,16 @@ var HomeView = function(store) {
 	    });
 	};
 
+	this.DisplayAll = function() {
+	    store.findByName($('*').val(), function(employees) {
+	        $('.employee-list').html(HomeView.liTemplate(employees));
+	    });
+	};
+
     this.initialize = function() {
         // Define a div wrapper for the view. The div wrapper is used to attach events.
         this.el = $('<div/>');
+        this.DisplayAll(); // Cargar toda la base de datos
         this.el.on('keyup', '.search-key', this.findByName);
     };
     this.initialize();
